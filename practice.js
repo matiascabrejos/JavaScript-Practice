fbe69d6d69ca0695b038b58c278a76714fe0665e
matiascabrejos <matias.cabrejos@hotmail.com>
2022-08-08 20:30:41 -0300

    updating

--- practice.js
+++ practice.js
@@ -47,6 +47,38 @@ isInt(5.5);
 
 // Q6: Implement enqueue and dequeue using only two stacks
 
+var inputStack = []; // First stack
+var outputStack = []; // Second stack
+
+// For enqueue, just push the item into the first stack
+function enqueue(stackInput, item) {
+  return stackInput.push(item);
+}
+
+function dequeue(stackInput, stackOutput) {
+  // Reverse the stack such that the first element of the output stack is the
+  // last element of the input stack. After that, pop the top of the output to
+  // get the first element that was ever pushed into the input stack
+  if (stackOutput.length <= 0) {
+    while (stackInput.length > 0) {
+      var elementToOutput = stackInput.pop();
+      stackOutput.push(elementToOutput);
+    }
+  }
+
+  return stackOutput.pop();
+}
+
+enqueue(inputStack, 5);
+enqueue(inputStack, 6);
+enqueue(inputStack, 7);
+
+dequeue(inputStack, outputStack);
+dequeue(inputStack, outputStack);
+
+console.log(inputStack);
+console.log(outputStack);
+
 // Q7: Make this work
 
 // Problem
@@ -106,4 +138,3 @@ isInt(5.5);
 // multiply(5)(6);
 
 // Q17: How does the this keyword work? Provide some code examples
-
