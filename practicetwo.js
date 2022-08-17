37ddc7427dd8428d4be7defe76d148cc3009012e
matiascabrejos <matias.cabrejos@hotmail.com>
2022-08-17 20:53:53 -0300

    updating

--- practicetwo.js
+++ practicetwo.js
@@ -62,6 +62,23 @@ c.retrieve(); // => The counter is currently at: 14
 
 // Q13: Provide some examples of non-bulean value coercion to a boolean one
 
+// The question is when a non-boolean value is coerced to a boolean, does it become true or false, respectively?
+
+// The specific list of "falsy" values in JavaScript is as follows:
+
+// "" (empty string)
+// 0, -0, NaN (invalid number)
+// null, undefined
+// false
+// Any value that's not on this "falsy" list is "truthy." Here are some examples of those:
+
+// "hello"
+// 42
+// true
+// [ ], [ 1, "2", 3 ] (arrays)
+// { }, { a: 42 } (objects)
+// function foo() { .. } (functions)
+
 // Q14: What will be the output of the following code ?
 
 // Problem
