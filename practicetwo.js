95653252d37a0386db5cb9c038fa60ca7bbac0f9
matiascabrejos <matias.cabrejos@hotmail.com>
2022-08-19 22:28:55 -0300

    updating

--- practicetwo.js
+++ practicetwo.js
@@ -89,6 +89,8 @@ c.retrieve(); // => The counter is currently at: 14
 // }
 // console.log(y);
 
+// Above code would give output 1undefined. If condition statement evaluate using eval so eval(function f() {}) which return function f() {} which is true so inside if statement code execute. typeof f return undefined because if statement code execute at run time, so statement inside if condition evaluated at run time.
+
 // Q15: What will the following code output ?
 
 // Problem
