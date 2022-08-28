d2c6231e01f68bb3a66bada8d441965854603e28
matiascabrejos <matias.cabrejos@hotmail.com>
2022-08-27 22:25:13 -0300

    updating

--- practicetwo.js
+++ practicetwo.js
@@ -119,6 +119,14 @@ console.log(multiply(5)(6));
 
 // https://www.freecodecamp.org/news/javascript-this-keyword-binding-rules/
 
+// To summarize,
+
+// In the case of implicit binding, this binds to the object adjacent to the dot(.) operator while invoking the method.
+// In the case of explicit binding, we can call a function with an object when the function is outside of the execution context of the object. The methods call(), apply(), and bind() play a big role here.
+// When a function is invoked with the new keyword, the this keyword inside the function binds to the new object being constructed.
+// When the this keyword is not resolved with any of the bindings, implicit, explicit or new, then this is bound to the window(global) object. In JavaScript's strict mode, this will be undefined.
+// In HTML event handlers, this binds to the HTML elements that receive the event.
+
 // Q18: How would you create a private variable in JavaScript?
 
 // https://css-tricks.com/implementing-private-variables-in-javascript/
