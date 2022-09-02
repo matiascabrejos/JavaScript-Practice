bd91c176be5973809e68f7611eb670c86f8723f3
matiascabrejos <matias.cabrejos@hotmail.com>
2022-09-02 19:03:53 -0300

    updating

--- practicethree.js
+++ practicethree.js
@@ -1,1 +1,11 @@
 // Another Q and Exercises
+// Q3. What are the data types supported by JavaScript?
+// The data types supported by JavaScript are:
+
+// Undefined
+// Null
+// Boolean
+// String
+// Symbol
+// Number
+// Object
