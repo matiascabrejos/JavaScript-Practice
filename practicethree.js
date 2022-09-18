af5c4ea9f4e7330435bab848fe20b7410ad358c0
matiascabrejos <matias.cabrejos@hotmail.com>
2022-09-17 23:20:09 -0300

    updating

--- practicethree.js
+++ practicethree.js
@@ -77,6 +77,12 @@
 // 1
 // document.cookie = "key1 = value1; key2 = value2; expires = date";
 
+// Q20. How to read a cookie using JavaScript?
+// Reading a cookie is just as simple as writing one, because the value of the document.cookie object is the cookie. So you can use this string whenever you want to access the cookie.
+
+// The document.cookie string will keep a list of name = value pairs separated by semicolons, where name is the name of a cookie and value is its string value.
+// You can use strings’ split() function to break the string into key and values.
+
 // Q21. How to delete a cookie using JavaScript?
 // If you want to delete a cookie so that subsequent attempts to read the cookie in JavaScript return nothing, you just need to set the expiration date to a time in the past. You should define the cookie path to ensure that you delete the right cookie. Some browsers will not let you delete a cookie if you don’t specify the path.
 
