dd17928b4915d3aaea520beb56f776bdcd5251fb
matiascabrejos <matias.cabrejos@hotmail.com>
2022-10-01 23:33:16 -0300

    updating

--- practicethree.js
+++ practicethree.js
@@ -105,6 +105,12 @@
 // External
 // An inline function is a JavaScript function, which is assigned to a variable created at runtime. You can differentiate between Inline Functions and Anonymous since an inline function is assigned to a variable and can be easily reused. When you need a JavaScript for a function, you can either have the script integrated in the page you are working on, or you can have it placed in a separate file that you call, when needed. This is the difference between an internal script and an external script.
 
+// Q26. What is a Typed language?
+// Typed Language is in which the values are associated with values and not with variables. It is of two types:
+
+// Dynamically: in this, the variable can hold multiple types; like in JS a variable can take number, chars.
+// Statically: in this, the variable can hold only one type, like in Java a variable declared of string can take only set of characters and nothing else.
+
 
 // Q27. What is the difference between Local storage & Session storage?
 // Storage - Edureka
