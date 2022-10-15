0cae1c35d4f9e210656d1ca7e3b626bd70ed254e
matiascabrejos <matias.cabrejos@hotmail.com>
2022-10-14 23:10:18 -0300

    updating

--- practicethree.js
+++ practicethree.js
@@ -28,6 +28,13 @@
 
 // Q6. What are the advantages of JavaScript?
 
+// Following are the advantages of using JavaScript −
+
+// Less server interaction − You can validate user input before sending the page off to the server. This saves server traffic, which means less load on your server.
+// Immediate feedback to the visitors − They don’t have to wait for a page reload to see if they have forgotten to enter something.
+// Increased interactivity − You can create interfaces that react when the user hovers over them with a mouse or activates them via the keyboard.
+// Richer interfaces − You can use JavaScript to include such items as drag-and-drop components and sliders to give a Rich Interface to your site visitors.
+
 // Q7. How can you create an object in JavaScript?
 // JavaScript supports Object concept very well. You can create an object using the object literal as follows −
 
