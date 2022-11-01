076eb105e708674f999f3f897fd35a9f60ccb06c
matiascabrejos <matias.cabrejos@hotmail.com>
2022-10-31 23:01:55 -0300

    mongodb updating

---
+++ practicefour.js
@@ -0,0 +1,7 @@
+Basic = Mongo DB
+db = Show name of current database
+mongod = Start database
+mongo = Connect to database
+show dbs = Show databases
+use db = Switch to database db
+show collec­tions = Display current database collec­tions
