ebb5324b281076406b6546ccdec8527a0585cfa0
matiascabrejos <matias.cabrejos@hotmail.com>
2022-11-01 22:31:41 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -1,7 +1,16 @@
-Basic = Mongo DB
-db = Show name of current database
-mongod = Start database
-mongo = Connect to database
-show dbs = Show databases
-use db = Switch to database db
-show collec­tions = Display current database collec­tions
+// Basic = Mongo DB
+
+// db = Show name of current database
+// mongod = Start database
+// mongo = Connect to database
+// show dbs = Show databases
+// use db = Switch to database db
+// show collec­tions = Display current database collec­tions
+
+
+// Create
+
+// insert­(data) = insert document(s) returns write result
+// insertOne (data, options) = insert one document
+// insert­Man­y(data, options) = insert many documents
+// insert­Man­y([­{},­{},{}]) = needs square brackets
