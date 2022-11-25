7f32e6a384f18a875759b36e8284ca66aa4ad873
matiascabrejos <matias.cabrejos@hotmail.com>
2022-11-24 23:10:55 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -57,3 +57,4 @@
 // syntax: = {key:{$in: [array of values] } }
 // $lt = Matches values that are less than a specified value.
 // $lte = Matches values that are less than or equal to a specified value.
+// $ne = Matches all values that are not equal to a specified value.
