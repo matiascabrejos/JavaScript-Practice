cedd5c470e68a7da2e07fb4a6f561093858f8625
matiascabrejos <matias.cabrejos@hotmail.com>
2022-11-28 23:10:07 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -61,3 +61,4 @@
 // $nin = Matches none of the values specified in an array.
 // $and = Performs AND operation
 // find({­doc.su­bdo­c:v­alue}) = Filter sub documents
+// {key: {$op: filter}, {filter}} = $and operator is necessary when the same field or operator has to be specified in multiple expres­sions
