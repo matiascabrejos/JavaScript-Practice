b7ce72c73a71276a714373a1bc4518dcf9f0f10c
matiascabrejos <matias.cabrejos@hotmail.com>
2022-12-29 22:37:27 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -88,3 +88,4 @@
 // https://cheatography.com/bligoubloups/cheat-sheets/heroku-nodejs-postgres/
 // https://cheatography.com/proloser/cheat-sheets/angularjs/
 // https://cheatography.com/spegusess/cheat-sheets/angular/
+// https://cheatography.com/nathane2005/cheat-sheets/angular-2-forms/
