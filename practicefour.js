2fe0bf64aaaca4b2324493018163ae99670fdfa2
matiascabrejos <matias.cabrejos@hotmail.com>
2023-01-20 22:51:27 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -112,4 +112,4 @@
 // https://cheatography.com/pyro19d/cheat-sheets/javascript/
 
 // Vue
-https://cheatography.com/peanuts-83/cheat-sheets/vue-js/
+// https://cheatography.com/peanuts-83/cheat-sheets/vue-js/
