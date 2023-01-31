3af1cd934db86cebdddf98b237079c19b3d5331c
matiascabrejos <matias.cabrejos@hotmail.com>
2023-01-30 22:42:38 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -118,6 +118,6 @@
 // React
 // https://cheatography.com/jaredcobb/cheat-sheets/es7-react-vscode-shortcuts/
 // https://cheatography.com/frankieali4/cheat-sheets/vscode-es7-react-redux-snippets/
-https://cheatography.com/kitallis/cheat-sheets/reactjs/
+// https://cheatography.com/kitallis/cheat-sheets/reactjs/
 
 
