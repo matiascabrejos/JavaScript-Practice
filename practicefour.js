2f595ec778801e7328be609d114eb1dd5e39fb3a
matiascabrejos <matias.cabrejos@hotmail.com>
2023-02-15 23:46:09 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -127,3 +127,4 @@
 // Nuxt
 // https://www.vuemastery.com/nuxt-cheat-sheet/
 // https://medium.com/js-dojo/the-ultimate-nuxt-js-cheat-sheet-96125109d8b8
+https://harlanzw.com/blog/nuxt-3-migration-cheatsheet
