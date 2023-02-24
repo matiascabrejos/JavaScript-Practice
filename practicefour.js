7831ba0754c100498fad6abb12af4c4cd2276b76
matiascabrejos <matias.cabrejos@hotmail.com>
2023-02-24 16:16:44 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -129,7 +129,7 @@
 // https://medium.com/js-dojo/the-ultimate-nuxt-js-cheat-sheet-96125109d8b8
 // https://harlanzw.com/blog/nuxt-3-migration-cheatsheet
 
-Tailwind
+// Tailwind
 // https://nerdcave.com/tailwind-cheat-sheet
 
 
