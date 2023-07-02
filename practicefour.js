374c1d2f49f9e736bf5e10b35bb2cad14702f931
matiascabrejos <matias.cabrejos@hotmail.com>
2023-07-01 23:12:22 -0300

    updating

--- practicefour.js
+++ practicefour.js
@@ -203,4 +203,4 @@
 // https://intellipaat.com/blog/tutorial/python-tutorial/pandas-cheat-sheet/?US
 // https://www.kaggle.com/code/themlphdstudent/cheatsheet-matplotlib-charts
 // https://figma-cheat-sheet.christianreich.art/
-https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+// https://kubernetes.io/docs/reference/kubectl/cheatsheet/
